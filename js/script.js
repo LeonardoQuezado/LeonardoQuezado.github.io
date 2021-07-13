1396f7bd330b646ff4f7c793033e30124553b0ed
Leo <leoquezado@outlook.com>
2021-07-13 18:01:54 -0300

    LoliPrivatier_beta_0.1

--- js/script.js
+++ js/script.js
@@ -28,3 +28,26 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+
+/* acordion skills*/
+const skillsContent = document.getElementsByClassName('skills__content')
+const skillsHeader = document.querySelectorAll('.skills__header')
+
+function toogleSkills(){
+    let itemClass = this.parentNode.className
+
+for(i = 0; i < skillsContent.length; i++){
+    skillsContent[i].className = 'skills__content skills__close'
+}
+
+if(itemClass === 'skills__content skills__close'){
+    this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toogleSkills)
+})
